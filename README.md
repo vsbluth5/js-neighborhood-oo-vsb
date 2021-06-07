60290208620d618d3f5b55f02c50f12c9ce9af71
Glitch (bedecked-shine-daphne) <none>
2021-06-07 18:59:28 +0000

    Import of upperlinecode/neighborhood-objects-javascript-oop

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Get Started!
+# Neighborhood Objects
 
-Add files and start typing 🎉
+[![Run on Repl.it](https://repl.it/badge/github/upperlinecode/neighborhood-objects-javascript-oop)](https://repl.it/github/upperlinecode/neighborhood-objects-javascript-oop)
+
+We're going to practice defining classes and creating instances of those classes. Open up the neighborhood.js file and let's get started.
---
+++ neighborhood.js
@@ -0,0 +1,58 @@
+// Level 1:
+// Create a class called Student that has two attributes: a name, and a grade.
+
+
+
+// Now create instances of three different students (student1, student2, and student3).
+
+
+
+// Confirm that the class works by printing out the first student's name.
+
+
+
+// Level 2:
+// Create a class called School that has three attributes: a name, a type, and a size.
+
+
+
+// Create instances of three individual schools.
+
+
+
+// Confirm that the class works by printing out the name and size of the third school.
+
+
+
+// Level 3:
+// Create a class called House that has four attributes: an address, a size, a price, and a number of bedrooms.
+
+
+
+// Create instances of at least three individual houses.
+
+
+
+// Confirm that the class works by printing out the address and size of the second house.
+
+
+
+// Level 4: (Stretch)
+// Put your three students in an array called myStudents, your houses in an array for houses, and your schools in an array for schools.
+
+
+
+// Iterate over the student array, printing out "_____ is in grade __." For each of the students.
+
+
+
+// Iterate over the houses array and print out a description for each one. Do the same for your schools array.
+
+
+
+// Level 5: (Stretch)
+// Modify your student class above to include a savingsAccount value for each student. Change your initializers so that the code still runs.
+
+
+
+// Write some code that compares a student and a house, and determines whether or not the student can afford to buy the house.
